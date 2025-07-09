4e444f11ccd27886a7caec33fd736d0300ba9c5c
ThiagoC0STA <thiago@lacostacorretora.com.br>
2025-07-09 00:29:56 -0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -47,6 +49,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
