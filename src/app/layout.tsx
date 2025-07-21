561d9b19a3410de7c5f521ca57099cf86caf582b
ThiagoC0STA <thiago@lacostacorretora.com.br>
2025-07-21 17:28:51 -0300

    meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -126,6 +126,7 @@ export default function RootLayout({
         <meta name="twitter:creator" content="@talentflight" />
 
         {/* AdSense */}
+        <meta name="google-adsense-account" content="ca-pub-7894980089417601" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7894980089417601"
